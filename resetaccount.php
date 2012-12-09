e1e6c68bea12b15748b771e45f23c51d1a2c57be
Ernest <glank314@gmail.com>
2012-12-09 14:03:18 -0500

    Updates and Pictures

--- resetaccount.php
+++ resetaccount.php
@@ -5,7 +5,9 @@
 	$con = mysql_connect("localhost","ejk314","password");
 	mysql_select_db("security", $con);
 	mysql_query("UPDATE people7489 SET Voted=0 WHERE Username='".$username."'",$con);
-	echo "Reset."
+	mysql_query("TRUNCATE TABLE votes9230",$con);
+	
+	echo "Reset.";
 ?>
 </body>
 </html>
